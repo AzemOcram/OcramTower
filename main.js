2e4b6028994d00f0ea1ae2441a23d693ee1d8918
crazy2be <code@jmcgirr.com>
2014-06-21 15:08:32 -0700

    Single canvas to renduce RAM usage.

--- main.js
+++ main.js
@@ -34,21 +34,18 @@ var ROOM_IMAGE_MAP = [
 	'Rooms/Default/MM_PrivateHousekeeping-day.png',
 ];
 
-window.onload = function() {
-	var renderedCtx = document.getElementById("renderedCanvas").getContext('2d');
-	var backgroundCtx = document.getElementById("backgroundCanvas").getContext('2d');
-	var towerCtx = document.getElementById("towerCanvas").getContext('2d');
-	var transportCtx = document.getElementById("transportCanvas").getContext('2d');
-
+var skyLine = new Image();
+var roomImages = [];
+function draw(ctx) {
 	// gradient 1, will eventually be removed
-	var bggradient = renderedCtx.createLinearGradient(30, 30, 90, 90);
+	var bggradient = ctx.createLinearGradient(30, 30, 90, 90);
 	bggradient.addColorStop(0, "#FF0000");
 	bggradient.addColorStop(1, "#00FF00");
-	renderedCtx.fillStyle = bggradient;
-	renderedCtx.fillRect(0, 0, 100, 100);
+	ctx.fillStyle = bggradient;
+	ctx.fillRect(0, 0, 100, 100);
 
 	//gradient 2, background
-	var skyGradient = backgroundCtx.createLinearGradient(0, 0, 0, 8200);
+	var skyGradient = ctx.createLinearGradient(0, 0, 0, 8200);
 	skyGradient.addColorStop(0, '#2040D0');
 	skyGradient.addColorStop(0.1, '#60A0D0');
 	skyGradient.addColorStop(0.2, '#62A1F0');
@@ -58,65 +55,77 @@ window.onload = function() {
 	skyGradient.addColorStop(0.904, '#A4CCF0');
 	skyGradient.addColorStop(0.904, '#A06018');
 	skyGradient.addColorStop(1, '#200D00');
-	backgroundCtx.fillStyle = skyGradient;
-	backgroundCtx.fillRect(0, 0, 7000, 8200);
+	ctx.fillStyle = skyGradient;
+	ctx.fillRect(0, 0, 7000, 8200);
 
-	var skyLine = new Image();
-
-	skyLine.onload = function () {
-		backgroundCtx.drawImage(skyLine, 0, 7125);
-		backgroundCtx.drawImage(skyLine, 1024, 7125);
-		backgroundCtx.drawImage(skyLine, 2048, 7125);
-		backgroundCtx.drawImage(skyLine, 3072, 7125);
-		backgroundCtx.drawImage(skyLine, 4096, 7125);
-		backgroundCtx.drawImage(skyLine, 4096, 7125);
-		backgroundCtx.drawImage(skyLine, 5120, 7125);
-		backgroundCtx.drawImage(skyLine, 6144, 7125);
-	};
-	skyLine.src = 'UI/OT_skyline.png';
+	ctx.drawImage(skyLine, 0, 7125);
+	ctx.drawImage(skyLine, 1024, 7125);
+	ctx.drawImage(skyLine, 2048, 7125);
+	ctx.drawImage(skyLine, 3072, 7125);
+	ctx.drawImage(skyLine, 4096, 7125);
+	ctx.drawImage(skyLine, 4096, 7125);
+	ctx.drawImage(skyLine, 5120, 7125);
+	ctx.drawImage(skyLine, 6144, 7125);
 
 	//gradient 3, to be removed eventually
-	var mygradient3 = towerCtx.createLinearGradient(60, 60, 120, 120);
+	var mygradient3 = ctx.createLinearGradient(60, 60, 120, 120);
 	mygradient3.addColorStop(0, "#0000FF");
 	mygradient3.addColorStop(.5, "#00FFDD");
-	towerCtx.fillStyle = mygradient3;
-	towerCtx.fillRect(60, 60, 120, 120);
-	renderIfLoaded.ctx = towerCtx;
-
-	var roomImages = renderIfLoaded.roomImages;
-	for (var i = 0; i < ROOM_IMAGE_MAP.length; i++) {
-		roomImages[i] = new Image();
-		roomImages[i].src = ROOM_IMAGE_MAP[i];
-		roomImages[i].onload = renderIfLoaded;
-	}
+	ctx.fillStyle = mygradient3;
+	ctx.fillRect(60, 60, 120, 120);
 
 	// gradient 4, to be removed eventually
-	var mygradient4 = transportCtx.createLinearGradient(30, 30, 90, 90);
+	var mygradient4 = ctx.createLinearGradient(30, 30, 90, 90);
 	mygradient4.addColorStop(0, "#DD33CC");
 	mygradient4.addColorStop(1, "#EEEEEE");
-	transportCtx.fillStyle = mygradient4;
-	transportCtx.fillRect(0, 0, 100, 100);
-}
+	ctx.fillStyle = mygradient4;
+	ctx.fillRect(0, 0, 100, 100);
 
-renderIfLoaded.roomImages = [];
-renderIfLoaded.ctx = null;
-renderIfLoaded.numLoaded = 0;
-function renderIfLoaded() {
-	renderIfLoaded.numLoaded++;
-	if (renderIfLoaded.numLoaded < ROOM_IMAGE_MAP.length - 1) {
-		return;
-	}
-	var posX = 1000;
+	// Draw all the rooms
+
+	var posX = 0;
 	var posY = 108;
-	var ctx = renderIfLoaded.ctx;
+	var ctx = ctx;
 	for (var i = 0; i < mapArray.length; i++) {
 		for (var j = 0; j < mapArray[i].length; j++) {
 			var roomIndex = mapArray[i][j];
-			var img = renderIfLoaded.roomImages[roomIndex];
+			var img = roomImages[roomIndex];
 			ctx.drawImage(img, posX, posY, img.naturalWidth, img.naturalHeight);
 			posX += 16;
 		}
-		posX = 1000
+		posX = 0
 		posY += 72;
+	}
+}
+
+window.onload = function() {
+	var ctx = document.getElementById("gameCanvas").getContext('2d');
+
+	skyLine.src = 'UI/OT_skyline.png';
+	waitFor(skyLine);
+
+	for (var i = 0; i < ROOM_IMAGE_MAP.length; i++) {
+		var roomImage = new Image();
+		roomImage.src = ROOM_IMAGE_MAP[i];
+		waitFor(roomImage);
+		roomImages.push(roomImage);
+	}
+
+	waitForAll(ctx);
+}
+
+waitFor.queue = [];
+waitFor.loaded = 0;
+function waitFor(img) {
+	waitFor.queue.push(img);
+}
+function waitForAll(ctx) {
+	for (var i = 0; i < waitFor.queue.length; i++) {
+		waitFor.queue[i].onload = function () {
+			waitFor.loaded++;
+			if (waitFor.loaded == waitFor.queue.length - 1) {
+				draw(ctx);
+			}
+		}
 	}
 }
